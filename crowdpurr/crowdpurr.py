89cedf7fd10b73f72d1dde4ed954dfaf73b77b2d
Martin McGreal <martin@mcgreal.org>
2018-09-26 16:33:26 -0500

    Fixed chart image sizing and cropping, added argparse

--- crowdpurr/crowdpurr.py
+++ crowdpurr/crowdpurr.py
@@ -19,6 +19,7 @@
 # ---
 
 
+import argparse
 import pandas as pd
 import seaborn as sns
 import numpy as np
@@ -28,19 +29,26 @@ from matplotlib import pyplot as plt
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, Image
 
+parser = argparse.ArgumentParser()
+parser.add_argument('datafile', type=str, help="Specify the data file")
+args = parser.parse_args()
+
 # Start by simply loading the data from CSV
-df = pd.read_csv('CrowdActivityExport_09132018_03-15PM.csv', true_values='Yes', false_values='No', na_values='-')
+df = pd.read_csv(args.datafile, true_values='Yes', false_values='No', na_values='-')
 
 # Loop through all the results, creating a box chart for each.
 for i in df['Question Number'].unique():
+    print("Processing question {}".format(i))
     q = df[df['Question Number'] == i][['Question Number', 'Vote Title', 'Question Title']]
-    fig = sns.catplot(y="Vote Title", kind="box", data=q)
+    fig, ax = plt.subplots(figsize=(3, 5))
+    sns.catplot(ax=ax, y="Vote Title", kind="box", data=q)
     plt.ylabel(q['Question Title'][:1].values[0])
 
     # OK, let's try a little PDF generation here...
     # Save the figure as a PNG so we can open it via PIL and convert it to a ReportLab flowable.
     filename = "figure-{}.png".format(i)
-    fig.savefig(filename)
+    fig.savefig(filename, bbox_inches='tight')
+    plt.close()
     im = PIL.Image.open(filename)
 
     buf = BytesIO()
